d81c148183f0065b66af25e116bb75954fcd0671
Vincent Gray <vincentveyron@aol.com>
2020-01-28 18:45:30 +0000

    create step method

--- model/agent.py
+++ model/agent.py
@@ -4,4 +4,9 @@ from mesa import Agent
 class HometimeAgent(Agent):
     """Agent in simulation"""
     def __init__(self, unique_id, model):
+        """Make a new agent"""
         super().__init__(unique_id, model)
+
+    def step(self):
+        """This is called on every time step"""
+        pass
