d26808ea70e9dd7820fba6ecaa592c1e65a70950
Vincent Gray <vincentveyron@aol.com>
2020-02-04 19:23:48 +0000

    database

---
+++ database/database_helper.py
@@ -0,0 +1,13 @@
+def create_restaurants_table(db_connection):
+    cursor = db_connection.cursor()
+    cursor.execute('DROP TABLE IF EXISTS restaurants')
+    cursor.execute('''CREATE TABLE restaurants
+        (name text, xpos real, ypos real)''')
+    db_connection.commit()
+
+
+def add_restaurant(db_connection, name, xpos, ypos):
+    cursor = db_connection.cursor()
+    cursor.execute('INSERT INTO restaurants VALUES (?,?,?)',
+                   name, xpos, ypos)
+    db_connection.commit()
