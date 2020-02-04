a67b8f5827897cf0a8518299e8bada2f0315577f
Vincent Gray <vincentveyron@aol.com>
2020-02-04 18:26:30 +0000

    add weather to utility function

--- model/agent.py
+++ model/agent.py
@@ -15,6 +15,16 @@ class HometimeAgent(Agent):
 
     def stage_one(self):
         print(str(self.unique_id) + " stage one")
+        utility = self.calculate_utility()
+        print(str(self.unique_id) + "'s utility is " + str(utility))
 
     def stage_two(self):
         print(str(self.unique_id) + " stage two")
+
+    def calculate_utility(self):
+        utility = 0
+        if self.model.good_weather:
+            utility += 0 * self.weather_weight
+        else:
+            utility += -1 * self.weather_weight
+        return utility
