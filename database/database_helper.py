faa719459447aba71363d901df8ab5fb16228f6c
Vincent Gray <vincentveyron@aol.com>
2020-02-04 19:50:11 +0000

    add some documentation

--- database/database_helper.py
+++ database/database_helper.py
@@ -1,4 +1,12 @@
 def create_restaurants_table(db_connection):
+    """
+    This creates a table to store restaurants in.
+    Caution this will delete the existing restaurant table.
+
+    Arguments
+    =========
+    - db_connection - database connection to an SQLite3 database
+    """
     cursor = db_connection.cursor()
     cursor.execute('DROP TABLE IF EXISTS restaurants')
     cursor.execute('''CREATE TABLE restaurants
@@ -7,6 +15,16 @@ def create_restaurants_table(db_connection):
 
 
 def add_restaurant(db_connection, name, xpos, ypos):
+    """
+    This adds a restaurant to the restaurants table.
+
+    Arguments
+    =========
+    - db_connection - database connection to an SQLite3 database
+    - name - name of the database
+    - xpos - position on the x axis
+    - ypos - position on the y axis
+    """
     cursor = db_connection.cursor()
     cursor.execute('INSERT INTO restaurants VALUES (?,?,?)',
                    name, xpos, ypos)
