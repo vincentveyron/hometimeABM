4415ae07323adb2e2f54e4a594e52f271d676d75
Vincent Gray <vincentveyron@aol.com>
2020-04-28 18:47:40 +0100

    revert last commit

--- database/database_helper.py
+++ database/database_helper.py
@@ -14,38 +14,6 @@ def create_restaurants_table(db_connection):
     db_connection.commit()
 
 
-def create_model_results_table(db_connection):
-    """
-    This creates a table to store the model_results in.
-    Caution this will delete the existing model_results table.
-
-    Arguments
-    =========
-    - db_connection - database connection to an SQLite3 database
-    """
-    cursor = db_connection.cursor()
-    cursor.execute('DROP TABLE IF EXISTS model_results')
-    cursor.execute('''CREATE TABLE model_results
-        (day integer, good_weather integer)''')
-    db_connection.commit()
-
-
-def create_agents_results_table(db_connection):
-    """
-    This creates a table to store the agents_results in.
-    Caution this will delete the existing model_results table.
-
-    Arguments
-    =========
-    - db_connection - database connection to an SQLite3 database
-    """
-    cursor = db_connection.cursor()
-    cursor.execute('DROP TABLE IF EXISTS agents_results')
-    cursor.execute('''CREATE TABLE agent_results
-        (day integer, agent_id integer, last_choice text)''')
-    db_connection.commit()
-
-
 def add_restaurant(db_connection, name, xpos, ypos):
     """
     This adds a restaurant to the restaurants table.
